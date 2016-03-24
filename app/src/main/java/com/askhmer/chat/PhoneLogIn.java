f92612f2c2f2c2b14fb57e476f296c4eff7b8f83
Thoeurn <th_kbt@yahoo.com>
2016-03-24 14:14:08 +0700

    HHHHH

--- app/src/main/java/com/askhmer/chat/PhoneLogIn.java
+++ app/src/main/java/com/askhmer/chat/PhoneLogIn.java
@@ -9,5 +9,7 @@ public class PhoneLogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_log_in);
+
+        echo "GGG";
     }
 }
