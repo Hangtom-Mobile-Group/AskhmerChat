0ae332fc89a4ba6795534e82076f133cd0835bd0
Longdy <longdygold@gmail.com>
2016-03-24 13:32:11 +0700

    first commit

---
+++ app/src/main/java/com/askhmer/chat/PhoneLogIn.java
@@ -0,0 +1,13 @@
+package com.askhmer.chat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PhoneLogIn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_log_in);
+    }
+}
