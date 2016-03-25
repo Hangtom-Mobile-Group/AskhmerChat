7fd80c5677cf0302e4f05996b926434563acd2e5
Longdy <longdygold@gmail.com>
2016-03-25 10:55:34 +0700

    Tab layout

--- app/src/main/java/com/askhmer/chat/activity/PhoneLogIn.java
+++ app/src/main/java/com/askhmer/chat/activity/PhoneLogIn.java
@@ -1,7 +1,9 @@
-package com.askhmer.chat;
+package com.askhmer.chat.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.askhmer.chat.R;
 
 public class PhoneLogIn extends AppCompatActivity {
 
@@ -9,6 +11,6 @@ public class PhoneLogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phone_log_in);
-        
+
     }
 }
---
+++ app/src/main/java/com/askhmer/chat/fragments/TwoFragment.java
@@ -0,0 +1,31 @@
+package com.askhmer.chat.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.askhmer.chat.R;
+
+public class TwoFragment extends Fragment {
+
+    public TwoFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        Log.d("Tab", "Tab2");
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_two, container, false);
+    }
+
+}
