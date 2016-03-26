4c32a84e9b5516b19b66b3ba8a6fbf104d9f3344
Longdy <longdygold@gmail.com>
2016-03-26 08:50:03 +0700

    Tab layout with floating btn 2

--- app/src/main/java/com/askhmer/chat/fragments/TwoFragment.java
+++ app/src/main/java/com/askhmer/chat/fragments/TwoFragment.java
@@ -3,22 +3,25 @@ package com.askhmer.chat.fragments;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.ViewCompat;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.OvershootInterpolator;
 
 import com.askhmer.chat.R;
-
+import com.github.clans.fab.FloatingActionMenu;
 public class TwoFragment extends Fragment  implements View.OnClickListener{
 
     private Boolean isFabOpen = false;
     private FloatingActionButton fab,fab1,fab2;
-    private Animation fab_open,fab_close,rotate_forward,rotate_backward;
+    private Animation fab_open,fab_close;
+
+    private com.github.clans.fab.FloatingActionButton fab12;
+    private com.github.clans.fab.FloatingActionButton fab22;
+
+    private View hideLayout;
+
 
     public TwoFragment() {
         // Required empty public constructor
@@ -36,24 +39,42 @@ public class TwoFragment extends Fragment  implements View.OnClickListener{
 
         View twoFragmentView = inflater.inflate(R.layout.fragment_two, container, false);
 
-        fab = (FloatingActionButton) twoFragmentView.findViewById(R.id.fab);
-        fab1 = (FloatingActionButton)twoFragmentView.findViewById(R.id.fab1);
-        fab2 = (FloatingActionButton)twoFragmentView.findViewById(R.id.fab2);
+        hideLayout = (View) twoFragmentView.findViewById(R.id.hiden_layout);
+
+        final FloatingActionMenu menu2 = (FloatingActionMenu) twoFragmentView.findViewById(R.id.menu2);
+
+        fab12 = (com.github.clans.fab.FloatingActionButton) twoFragmentView.findViewById(R.id.fab12);
+        fab22 = (com.github.clans.fab.FloatingActionButton) twoFragmentView.findViewById(R.id.fab22);
+
+        menu2.setOnMenuToggleListener(new FloatingActionMenu.OnMenuToggleListener() {
+            @Override
+            public void onMenuToggle(boolean opened) {
+                if (opened) {
+                    hideLayout.setVisibility(View.VISIBLE);
+                } else {
+                    hideLayout.setVisibility(View.INVISIBLE);
+                }
+            }
+        });
+
+        fab12.setOnClickListener(this);
+        fab22.setOnClickListener(this);
 
 
-//        fab1.setImageDrawable(new TextDrawable(fab.getContext(), "Group chat", ColorStateList.valueOf(Color.BLACK), 32.f, TextDrawable.VerticalAlignment.BASELINE));
-//        fab2.setImageDrawable(new TextDrawable(fab.getContext(), "Secret chat", ColorStateList.valueOf(Color.BLACK), 32.f, TextDrawable.VerticalAlignment.BASELINE));
 
-
-        rotate_forward = AnimationUtils.loadAnimation(getActivity(),R.anim.rotate_forward);
-        rotate_backward = AnimationUtils.loadAnimation(getActivity(),R.anim.rotate_backward);
-
+/*
         fab_open = AnimationUtils.loadAnimation(getActivity(), R.anim.fab_open);
         fab_close = AnimationUtils.loadAnimation(getActivity(),R.anim.fab_close);
 
+        fab = (FloatingActionButton) twoFragmentView.findViewById(R.id.fab);
+        fab1 = (FloatingActionButton)twoFragmentView.findViewById(R.id.fab1);
+        fab2 = (FloatingActionButton)twoFragmentView.findViewById(R.id.fab2);
+*/
+/*
         fab.setOnClickListener(this);
         fab1.setOnClickListener(this);
         fab2.setOnClickListener(this);
+*/
 
         // Inflate the layout for this fragment
         return twoFragmentView;
@@ -63,8 +84,9 @@ public class TwoFragment extends Fragment  implements View.OnClickListener{
     @Override
     public void onClick(View v) {
         int id = v.getId();
+        String text = "";
         switch (id){
-            case R.id.fab:
+/*            case R.id.fab:
 
                 animateFAB();
                 break;
@@ -76,9 +98,16 @@ public class TwoFragment extends Fragment  implements View.OnClickListener{
 
                 Log.d("fab", "Fab 2");
                 break;
+*/
+            case R.id.fab12:
+                text = fab12.getLabelText();
+                break;
+            case R.id.fab22:
+                text = fab22.getLabelText();
+                break;
         }
     }
-
+/*
     public void animateFAB(){
 
         if(isFabOpen)
@@ -104,8 +133,9 @@ public class TwoFragment extends Fragment  implements View.OnClickListener{
             Log.d("fab","open");
         }
     }
+*/
 
-
+/*
     public void rotateFabForward() {
         ViewCompat.animate(fab)
                 .rotation(45.0F)
@@ -123,4 +153,6 @@ public class TwoFragment extends Fragment  implements View.OnClickListener{
                 .setInterpolator(new OvershootInterpolator(5.0F))
                 .start();
     }
+*/
+
 }
