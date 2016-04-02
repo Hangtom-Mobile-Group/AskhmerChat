60617e62efd1e6e87be4796ae9b36896ca0861bf
lim ravy <limravytop10@gmail.com>
2016-04-02 08:49:48 +0700

    ravy commit now

--- app/src/main/java/com/askhmer/chat/fragments/OneFragment.java
+++ app/src/main/java/com/askhmer/chat/fragments/OneFragment.java
@@ -1,18 +1,19 @@
 package com.askhmer.chat.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
-
 import com.askhmer.chat.R;
-import com.askhmer.chat.adapter.ContactAdapter;
-import com.askhmer.chat.listener.ClickListener;
-import com.askhmer.chat.listener.RecyclerItemClickListenerInFragment;
+import com.askhmer.chat.adapter.AddfriendAdapter;
+import com.askhmer.chat.model.Friends;
 import com.askhmer.chat.model.Contact;
 
 import java.util.ArrayList;
@@ -21,9 +22,9 @@ import java.util.List;
 
 public class OneFragment extends Fragment {
 
-    private List<Contact> contactList = new ArrayList<>();
+    private List<Friends> addfriendtList = new ArrayList<>();
     private RecyclerView recyclerView;
-    private ContactAdapter mAdapter;
+    private AddfriendAdapter fAdapter;
 
     public OneFragment() {
         // Required empty public constructor
@@ -32,8 +33,7 @@ public class OneFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
+        prepareAddfriendData();
     }
 
     @Override
@@ -46,26 +46,22 @@ public class OneFragment extends Fragment {
 
         recyclerView = (RecyclerView) oneFragmentView.findViewById(R.id.recycler_view);
 
-        mAdapter = new ContactAdapter(contactList);
+        fAdapter = new AddfriendAdapter(addfriendtList);
 
         recyclerView.setHasFixedSize(true);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+
         recyclerView.setLayoutManager(mLayoutManager);
        // recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
        // recyclerView.setItemAnimator(new DefaultItemAnimator());
-        recyclerView.setAdapter(mAdapter);
+        recyclerView.setAdapter(fAdapter);
 
-        //list item
-        for (int i = 0; i < 15; i++) {
-            Contact item = new Contact("Friend : "+i,"Chat Id : 000"+i);
-            contactList.add(item);
-        }
 
-        recyclerView.addOnItemTouchListener(new RecyclerItemClickListenerInFragment(getActivity(), recyclerView, new ClickListener() {
+        recyclerView.addOnItemTouchListener(new OneFragment.RecyclerTouchListener(getActivity(), recyclerView, new OneFragment.ClickListener() {
             @Override
-            public void onClick(View view, int position)    {
-                Contact movie = contactList.get(position);
-                Toast.makeText(getActivity(), movie.getName() + " is selected!", Toast.LENGTH_SHORT).show();
+            public void onClick(View view, int position) {
+                Friends movie = addfriendtList.get(position);
+                Toast.makeText(getActivity(), movie.getFriName() + " is selected!", Toast.LENGTH_SHORT).show();
             }
 
             @Override
@@ -73,69 +69,70 @@ public class OneFragment extends Fragment {
 
             }
         }));
-/*
 
-        final Toolbar toolbar = (Toolbar) oneFragmentView.findViewById(R.id.toolbar);
-
-        recyclerView.setOnScrollListener(new MyScrollListener(getContext()) {
-            @Override
-            public void onMoved(int distance) {
-                toolbar.setTranslationY(-distance);
-            }
-        });
-*/
-
-//        prepareMovieData();
         return oneFragmentView;
     }
 
 
-    private void prepareMovieData() {
-/*        Contact contact = new Contact("Mad Max: Fury Road", "Action & Adventure");
-        contactList.add(contact);
-        contact = new Contact("Inside Out", "Animation, Kids & Family");
-        contactList.add(contact);
+    private void prepareAddfriendData() {
 
-        contact = new Contact("Shaun the Sheep", "Animation");
-        contactList.add(contact);
+        for(int i=0;i<=20;i++) {
+            Friends addfriend = new Friends();
+            addfriend.setFriName("Lim Ravy");
+            addfriend.setChatId("xyz123hangtom");
+            addfriendtList.add(addfriend);
 
-        contact = new Contact("The Martian", "Science Fiction & Fantasy");
-        contactList.add(contact);
+        }
+        /// fAdapter.notifyDataSetChanged();
+    }
 
-        contact = new Contact("Mission: Impossible Rogue Nation", "Action");
-        contactList.add(contact);
+    public interface ClickListener {
+        void onClick(View view, int position);
 
-        contact = new Contact("Up", "Animation");
-        contactList.add(contact);
+        void onLongClick(View view, int position);
+    }
 
-        contact = new Contact("Star Trek", "Science Fiction");
-        contactList.add(contact);
+    public static class RecyclerTouchListener implements RecyclerView.OnItemTouchListener {
 
-        contact = new Contact("The LEGO Movie", "Animation");
-        contactList.add(contact);
+        private GestureDetector gestureDetector;
+        private OneFragment.ClickListener clickListener;
 
-        contact = new Contact("Iron Man", "Action & Adventure");
-        contactList.add(contact);
+        public RecyclerTouchListener(Context context, final RecyclerView recyclerView, final OneFragment.ClickListener clickListener) {
+            this.clickListener = clickListener;
+            gestureDetector = new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+                @Override
+                public boolean onSingleTapUp(MotionEvent e) {
+                    return true;
+                }
 
-        contact = new Contact("Aliens", "Science Fiction");
-        contactList.add(contact);
+                @Override
+                public void onLongPress(MotionEvent e) {
+                    View child = recyclerView.findChildViewUnder(e.getX(), e.getY());
+                    if (child != null && clickListener != null) {
+                        clickListener.onLongClick(child, recyclerView.getChildPosition(child));
+                    }
+                }
+            });
+        }
 
-        contact = new Contact("Chicken Run", "Animation");
-        contactList.add(contact);
+        @Override
+        public boolean onInterceptTouchEvent(RecyclerView rv, MotionEvent e) {
 
-        contact = new Contact("Back to the Future", "Science Fiction");
-        contactList.add(contact);
+            View child = rv.findChildViewUnder(e.getX(), e.getY());
+            if (child != null && clickListener != null && gestureDetector.onTouchEvent(e)) {
+                clickListener.onClick(child, rv.getChildPosition(child));
+            }
+            return false;
+        }
 
-        contact = new Contact("Raiders of the Lost Ark", "Action & Adventure");
-        contactList.add(contact);
+        @Override
+        public void onTouchEvent(RecyclerView rv, MotionEvent e) {
+        }
 
-        contact = new Contact("Goldfinger", "Action & Adventure");
-        contactList.add(contact);
+        @Override
+        public void onRequestDisallowInterceptTouchEvent(boolean disallowIntercept) {
 
-        contact = new Contact("Guardians of the Galaxy", "Science Fiction & Fantasy");
-        contactList.add(contact);*/
-
-        mAdapter.notifyDataSetChanged();
+        }
     }
 
 }
