1bbbc078f98036de84c23ff6c3a7ab45899206ae
lim ravy <limravytop10@gmail.com>
2016-04-04 09:31:11 +0700

    add more commit

--- app/src/main/java/com/askhmer/chat/fragments/FourFragment.java
+++ app/src/main/java/com/askhmer/chat/fragments/FourFragment.java
@@ -34,36 +34,61 @@ public class FourFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        Log.d("Tab", "Tab4");
-        // Inflate the layout for this fragment
-        View fourFragmentView = inflater.inflate(R.layout.fragment_four, container, false);
-
 
         ContentResolver resolver = getActivity().getContentResolver();
         Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
 
-        while (cursor.moveToNext()){
-            String id =  cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+        while (cursor.moveToNext()) {
+            String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
             String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Profile.DISPLAY_NAME));
 
             Contact item = new Contact();
-            Cursor phoneCursor =   resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+            Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
                     ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
 //             Log.i("INFO  : ",  id + " = " + name);
 
-            while (phoneCursor.moveToNext()){
-                String phoneNumber =   phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+            while (phoneCursor.moveToNext()) {
+                String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
                 Log.i("INFO  : ", name + " = " + phoneNumber);
 
                 item.setName(name);
                 item.setPhoneNumber(phoneNumber);
                 contactList.add(item);
             }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+
+
+            Log.d("Tab", "Tab4");
+            // Inflate the layout for this fragment
+            View fourFragmentView = inflater.inflate(R.layout.fragment_four, container, false);
+
+//
+//            ContentResolver resolver = getActivity().getContentResolver();
+//            Cursor cursor = resolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+//
+//            while (cursor.moveToNext()) {
+//                String id = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+//                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Profile.DISPLAY_NAME));
+//
+//                Contact item = new Contact();
+//                Cursor phoneCursor = resolver.query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,
+//                        ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ?", new String[]{id}, null);
+////             Log.i("INFO  : ",  id + " = " + name);
+//
+//                while (phoneCursor.moveToNext()) {
+//                    String phoneNumber = phoneCursor.getString(phoneCursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+//                    Log.i("INFO  : ", name + " = " + phoneNumber);
+//
+//                    item.setName(name);
+//                    item.setPhoneNumber(phoneNumber);
+//                    contactList.add(item);
+//                }
 /*
             Cursor emailCursor = resolver.query(ContactsContract.CommonDataKinds.Email.CONTENT_URI,null,
                     ContactsContract.CommonDataKinds.Email.CONTACT_ID + " = ? ", new String[] {id}, null);
@@ -74,18 +99,18 @@ public class FourFragment extends Fragment {
                   Log.i("email :", email);
             }
 */
+
+            recyclerView = (RecyclerView) fourFragmentView.findViewById(R.id.friend_in_contact);
+
+            mAdapter = new ContactAdapter(contactList);
+
+            RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
+            recyclerView.setLayoutManager(mLayoutManager);
+            recyclerView.setAdapter(mAdapter);
+
+            return fourFragmentView;
+
         }
-        recyclerView = (RecyclerView) fourFragmentView.findViewById(R.id.friend_in_contact);
 
-        mAdapter = new ContactAdapter(contactList);
-
-        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(mLayoutManager);
-        recyclerView.setAdapter(mAdapter);
-
-
-
-        return fourFragmentView;
-    }
 
 }
