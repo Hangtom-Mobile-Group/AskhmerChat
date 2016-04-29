06cb016d319022fb01f93ab4dc42addb42f75a10
Thoeurn <th_kbt@yahoo.com>
2016-04-29 13:18:45 +0700

    DDDDD

---
+++ app/src/main/java/com/askhmer/chat/model/DataFriends.java
@@ -0,0 +1,29 @@
+package com.askhmer.chat.model;
+
+/**
+ * Created by Thoeurn on 4/25/2016.
+ */
+public class DataFriends {
+
+    private String id;
+    private String name;
+
+    public DataFriends(String id, String name){
+        this.id = id;
+        this.name = name;
+    }
+
+    public void setId(String id){
+        this.id = id;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
+    }
+}
