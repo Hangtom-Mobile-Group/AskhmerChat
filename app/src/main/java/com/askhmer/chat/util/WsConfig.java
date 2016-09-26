87475501df8df61c2ffb6c8a0e49f70f8ebfe3da
phalleak <lachphalleak70@gmail.com>
2016-09-26 16:15:09 +0700

    Phalleak

--- app/src/main/java/com/askhmer/chat/util/WsConfig.java
+++ app/src/main/java/com/askhmer/chat/util/WsConfig.java
@@ -1,6 +1,6 @@
 package com.askhmer.chat.util;
 
 public class WsConfig {
-	//public static final String URL_WEBSOCKET = "ws://chat.askhmer.com:8080/chating";
-	public static final String URL_WEBSOCKET = "ws://192.168.1.103:8080/ChatAskhmer/chating";
+	public static final String URL_WEBSOCKET = "ws://chat.askhmer.com:8080/chating";
+	//public static final String URL_WEBSOCKET = "ws://192.168.1.103:8080/ChatAskhmer/chating";
 }
