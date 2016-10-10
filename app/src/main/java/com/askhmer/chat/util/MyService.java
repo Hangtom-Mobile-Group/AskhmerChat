beeec808244a157a726aaeda6d7d7814f10c8485
phalleak <lachphalleak70@gmail.com>
2016-10-10 17:11:02 +0700

    Ok2

--- app/src/main/java/com/askhmer/chat/util/MyService.java
+++ app/src/main/java/com/askhmer/chat/util/MyService.java
@@ -12,9 +12,17 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.askhmer.chat.network.GsonObjectRequest;
+import com.askhmer.chat.network.MySingleton;
+
 import org.java_websocket.client.WebSocketClient;
 import org.java_websocket.drafts.Draft_17;
 import org.java_websocket.handshake.ServerHandshake;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.URI;
@@ -83,6 +91,7 @@ public class MyService  extends Service{
                           SharedPreferencesFile  mSharedPrefer = SharedPreferencesFile.newInstance(context.getApplicationContext(), SharedPreferencesFile.PREFER_FILE_NAME);
                           final String user_id = mSharedPrefer.getStringSharedPreference(SharedPreferencesFile.USERIDKEY);
                           if(user_id != null){
+                              listNotSeenMessage(user_id,context);
                               URI uri=null;
                               try {
                                   uri=new URI(WsConfig.URL_WEBSOCKET);
@@ -215,4 +224,45 @@ public class MyService  extends Service{
         return group_id;
     }
 
+
+
+    private void listNotSeenMessage(String userId, final Context context) {
+
+        String url = "http://chat.askhmer.com/api/message/notiftynotseen/"+userId;
+        GsonObjectRequest jsonRequest = new GsonObjectRequest(Request.Method.GET,url,new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                String imageResource="http://chat.askhmer.com/resources/upload/file/";
+                String []param={};
+                try {
+                    if (response.has("DATA")) {
+                        JSONArray jsonArray = response.getJSONArray("DATA");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                                    int userid=jsonArray.getJSONObject(i).getInt("userId");
+                                    String username=jsonArray.getJSONObject(i).getString("userName");
+                                    String image_url=jsonArray.getJSONObject(i).getString("userProfile");
+                                    String message=jsonArray.getJSONObject(i).getString("message");
+                                    int groupid= jsonArray.getJSONObject(i).getInt("roomId");
+                            if(image_url.contains("facebook")){
+                                param[0]=image_url;
+                            }else{
+                                param[0]=imageResource+image_url;
+                            }
+                            new NotificationGenerator(context,message,username,groupid,userid,param[0]).execute(param);
+                        }
+                    }else{
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                // CustomDialog.hideProgressDialog();
+//                Toast.makeText(getContext(),"Error", Toast.LENGTH_LONG).show();
+            }
+        });
+        MySingleton.getInstance(context).addToRequestQueue(jsonRequest);
+    }
 }
